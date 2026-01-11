88eec9a44666ecc276b40cd78661c842852fc033
Erick Farias <erick.off09@gmail.com>
2026-01-11 20:58:25 -0300

    Adicionando atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,37 +1,19 @@
-# 1 - Programa de Saudação
 print("Olá, mundo!")
 
-print("---------------------------")
-
-# 2 - Calculadora de Soma
 numero1 = 12
 numero2 = 14
-
 soma = numero1 + numero2
+print(soma)
 
-print("A soma é:", soma)
-
-print("---------------------------")
-
-# 3 - Calculadora de Volume
 comprimento = 12
 largura = 14
 altura = 20
-
 volume = comprimento * largura * altura
+print(volume)
 
-print("O volume da caixa é:", volume, "cm³")
-
-print("---------------------------")
-
-# 4 - Calculadora de Preço Total
-nome_produto = "Cadeira Infantil"
-preco_unitario = 12.40
+nome = "Cadeira Infantil"
+preco = 12.40
 quantidade = 3
-
-preco_total = preco_unitario * quantidade
-
-print("Produto:", nome_produto)
-print("Preço unitário: R$", preco_unitario)
-print("Quantidade:", quantidade)
-print("Preço total: R$", preco_total)
+total = preco * quantidade
+print(nome)
+print(total)
